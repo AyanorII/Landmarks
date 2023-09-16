11817f2faee4395f1d546103f8b602cf48c2b3c2
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2023-09-16 11:13:52 +0900

    🔧 refactor(CircleImage.swift): replace hardcoded image with variable for flexibility 🔧 refactor(LandmarkDetail.swift): replace hardcoded landmark details with variable for dynamic content 🔧 refactor(LandmarkList.swift): pass landmark data to LandmarkDetail view for accurate display 🔧 refactor(MapView.swift): replace hardcoded coordinates with variable for dynamic map display

--- Landmarks/Views/LandmarkDetail.swift
+++ Landmarks/Views/LandmarkDetail.swift
@@ -8,44 +8,44 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    var landmark: Landmark
+    
     var body: some View {
-        VStack {
-            MapView()
+        ScrollView {
+            MapView(coordinate: landmark.locationCoordinate)
                 .frame(height: 300)
                 .ignoresSafeArea(edges: .top)
             
-            CircleImage()
+            CircleImage(image: landmark.image)
                 .offset(y: -180)
                 .padding(.bottom, -180)
             
             VStack(alignment: .leading) {
-                Text("Hello, world!")
+                Text(landmark.name)
                     .font(.title)
                 
                 HStack {
-                    Text("Joshua Tree National Park")
+                    Text(landmark.park)
                     Spacer()
-                    Text("California")
+                    Text(landmark.state)
                 }
                 .font(.subheadline)
                 .foregroundColor(.secondary)
                 
                 Divider()
                 
-                Text("About Turtle Rock")
+                Text("About \(landmark.name)")
                     .font(.title2)
-                Text("Descriptive text goes here.")
+                Text(landmark.description)
                 
             }
             .padding()
-            
-            Spacer()
         }
     }
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkDetail()
+        LandmarkDetail(landmark: landmarks[0])
     }
 }

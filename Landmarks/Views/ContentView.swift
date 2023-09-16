a82f55f2c31734349ebcfaab7e89dd9bb65100cc
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2023-09-16 10:54:49 +0900

    ✨ feat(Landmarks): add LandmarkDetail view to separate detail view from ContentView 🔨 refactor(ContentView): replace existing code with LandmarkList to improve modularity and readability

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -9,39 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            MapView()
-                .frame(height: 300)
-                .ignoresSafeArea(edges: .top)
-            
-            CircleImage()
-                .offset(y: -180)
-                .padding(.bottom, -180)
-            
-            VStack(alignment: .leading) {
-                Text("Hello, world!")
-                    .font(.title)
-                
-                HStack {
-                    Text("Joshua Tree National Park")
-                    Spacer()
-                    Text("California")
-                }
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-                
-                Divider()
-                
-                Text("About Turtle Rock")
-                    .font(.title2)
-                Text("Descriptive text goes here.")
-                
-            }
-            .padding()
-            
-            Spacer()
-            
-        }
+        LandmarkList()
     }
 }
 
